f36a0f55604f40e5477a9c384c204076898cfd7e
Jed Liu <liujed@users.noreply.github.com>
2021-03-30 14:44:07 -0700

    Model specs

--- spec/models/aki_file_spec.rb
+++ spec/models/aki_file_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
+# Test suite for the AkiFile model
 RSpec.describe AkiFile, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Association test
+  # Ensure each AkiFile belongs to a single User
+  it { should belong_to(:user) }
+
+  # Validation test
+  # Ensure all fields are populated before saving.
+  it { should validate_presence_of(:filename) }
+  it { should validate_presence_of(:contents) }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
+# Test suite for the User model
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Association test.
+  # Ensure User model has a one-to-many relationship with the AkiFile model.
+  it { should have_many(:aki_files).dependent(:destroy) }
+
+  # Validation tests.
+  # Ensure all fields are populated before saving.
+  it { should validate_presence_of(:first_name) }
+  it { should validate_presence_of(:last_name) }
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:phone_number) }
 end
