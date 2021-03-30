f468a7c50287350224dac450cb87bb4b6da7ffc8
Jed Liu <liujed@users.noreply.github.com>
2021-03-30 14:44:21 -0700

    Fix test failures

--- app/models/aki_file.rb
+++ app/models/aki_file.rb
@@ -1,3 +1,7 @@
 class AkiFile < ApplicationRecord
+  # Model association.
   belongs_to :user
+
+  # Validations.
+  validates_presence_of :filename, :contents
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  # Model association.
+  has_many :aki_files, dependent: :destroy
+
+  # Validations.
+  validates_presence_of :first_name, :last_name, :email, :phone_number
 end
