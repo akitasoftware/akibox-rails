129b5b81e04edf7d1dd03bcc45aed4fc54834315
Jed Liu <liujed@users.noreply.github.com>
2021-04-12 16:55:52 -0700

    Responding to user feedback
    Removed extraneous `require`.
    
    Moved installation of integration to `config/environments/test.rb`.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -56,4 +56,7 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
+
+  # Put the HAR logger at the top of the middleware stack.
+  Akita::HarLogger.instrument(config)
 end
